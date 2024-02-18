46342fdb65b1745e3ba1a79f6794bdd7c012de27
ajaykumar2pp <ajay2kumarpp@gmail.com>
2024-02-18 10:53:47 +0530

    update dataAPI url

--- src/api/dataAPI.js
+++ src/api/dataAPI.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8500/",
+  // baseURL: "http://localhost:8500/",
+  baseURL: "https://transaction-application.onrender.com",
 });
